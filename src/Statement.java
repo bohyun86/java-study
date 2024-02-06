6394157e4a99d9efddb8c219e10a4b42d6ffbce1
bohyun86 <bohyun86@kakao.com>
2024-02-06 12:49:45 +0900

    Initial commit

---
+++ src/Statement.java
@@ -0,0 +1,12 @@
+public class Statement {
+    public static void main(String[] args) {
+        // 실행문 끝에는 반드시 세미콜론을 붙여서 실행문이 끝났음을 표시해야 한다.
+        System.out.println("Hello");
+
+        int x; // x라는 변수를 선언
+        x = 1; // x에 1을 저장해라.
+        int y = 2; // y를 선언하고 2를 저장해라
+        int result = x + y; // result를 선언하고 x와 y를 더해서 저장해라
+        System.out.println(result);
+    }
+}
