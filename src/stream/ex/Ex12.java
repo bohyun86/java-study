bf103890647a4416e3210947ae5b95892a5e3002
bohyun86 <bohyun86@kakao.com>
2024-03-20 16:30:10 +0900

    3/19 Lambda

---
+++ src/stream/ex/Ex12.java
@@ -0,0 +1,32 @@
+package stream.ex;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Ex12 {
+    public static void main(String[] args) {
+        record Employee(String name, String role, int salary) {
+        }
+
+
+        List<Employee> employees = Arrays.asList(
+                new Employee("Alice", "Developer", 5000),
+                new Employee("Bob", "Developer", 6000),
+                new Employee("Charlie", "Manager", 7000),
+                new Employee("David", "Manager", 8000)
+        );
+
+        int sum = employees.stream().mapToInt(Employee::salary).sum();
+        System.out.println("총 급여: " + sum);
+
+        Map<String, Double> collect = employees.stream()
+                .collect(Collectors.groupingBy(Employee::role, Collectors.averagingDouble(Employee::salary)));
+        Double developer = collect.get("Developer");
+        Double Manager = collect.get("Manager");
+
+        System.out.printf("Developer 평균 급여: %.1f\n", developer);
+        System.out.printf("Manager 평균 급여: %.1f\n", Manager);
+    }
+}
---
+++ src/stream/ex/Ex13.java
@@ -0,0 +1,35 @@
+package stream.ex;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Ex13 {
+    public static void main(String[] args) {
+        record CityPopulation(String city, int population) {}
+
+        List<CityPopulation> cityPopulations = Arrays.asList(
+                new CityPopulation("New York", 8000000),
+                new CityPopulation("Los Angeles", 4000000),
+                new CityPopulation("Chicago", 2700000),
+                new CityPopulation("New York", 7500000),
+                new CityPopulation("Los Angeles", 3800000),
+                new CityPopulation("Chicago", 2600000)
+        );
+
+
+        Map<String, Double> collect = cityPopulations.stream()
+                .collect(Collectors.groupingBy(CityPopulation::city, Collectors.averagingDouble(CityPopulation::population)));
+
+        Double newYork = collect.get("New York");
+        Double Chicago = collect.get("Chicago");
+        Double LosAngeles = collect.get("Los Angeles");
+
+        System.out.printf("New York 평균 인구: %,.0f\n", newYork);
+        System.out.printf("Chicago 평균 인구: %,.0f\n", Chicago);
+        System.out.printf("LosAngeles 평균 인구: %,.0f\n", LosAngeles);
+
+    }
+
+}
